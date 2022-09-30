2951067b6c666354a205d1f4b5e76623eb17ee28
igotyb <igotyb.stream@gmail.com>
2022-09-30 23:36:21 +0200

    exercises about JavaScript to learn the basics

---
+++ 4. JavaScript/scriptVariable.js
@@ -0,0 +1,48 @@
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// console.log(c);
+// var trueCalcul = a < b;
+// console.log(trueCalcul);
+// var type = typeof trueCalcul;
+// console.log(type);
+
+// console.log(`Si je fais ${a} + ${b}, j'obtiens ${c}.`);
+/*
+=========================================================================
+* alert permet d'afficher une box à l'écran
+* prompt affiche une box à l'écran, avec entrée
+=========================================================================
+*/
+
+// alert('Coucou');
+// prompt('Comment ça va ?')
+
+/*
+=========================================================================
+* typeof permet de retourner le type de notre variable
+* Number() permet de changer le type de variable
+* créer une var avec un prompt permet de stocker la valeur de la réponse
+* var nombreDeux = Number(prompt('Votre deuxième nombre'));
+=========================================================================
+*/
+
+// alert("Bienvenu dans le programme d'addition");
+// var nombreUn = prompt('Votre premier nombre');
+// var nombreDeux = prompt('Votre deuxième nombre');
+
+// alert(typeof nombreUn);
+// alert(typeof Number(nombreUn));
+// alert(Number(nombreUn) + Number(nombreDeux));
+
+/*
+=========================================================================
+* parseInt ne prend en considération que le nombre entier
+=========================================================================
+*/
+
+// alert("Bienvenu dans le programme d'addition");
+// var nombreUn = prompt('Votre premier nombre');
+// var nombreDeux = prompt('Votre deuxième nombre');
+// alert(parseInt(nombreUn) + parseInt(nombreDeux));
+

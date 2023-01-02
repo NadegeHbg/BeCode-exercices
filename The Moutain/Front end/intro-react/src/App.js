463e66fd4e4a181759608daeb57c89301b62cee7
IGotYB <igotyb.stream@gmail.com>
2023-01-02 16:58:51 +0100

    separation of components

--- The Moutain/Front end/intro-react/src/App.js
+++ The Moutain/Front end/intro-react/src/App.js
@@ -1,32 +1,51 @@
 
 import './App.css';
 
+const Title = () => {
+  return <h1 className='App-Title'>My Todo App</h1>;
+
+}
+const Form = () => {
+  return (
+    <form>
+      <div className='displayFlex'>
+        <input type='text' placeholder='Type a new todo' />
+        <input type='submit' />
+      </div>
+    </form>)
+}
+
+const SubTitle = () => {
+  return <h2>Todos</h2>
+}
+
+const TodosList = () => {
+  return (
+    <div className='displayFlex alignStart'>
+      <label>
+        <input type='checkbox' />
+        <span>Be awesome</span>
+      </label>
+      <label>
+        <input type='checkbox' />
+        <span>Learn react</span>
+      </label>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='App-Title'>My Todo App</h1>
+        <Title />
       </header>
+
       <main className="App-body">
+        <Form />
+        <SubTitle />
+        <TodosList />
 
-        <form>
-          <div className='displayFlex'>
-            <input type='text' placeholder='Type a new todo' />
-            <input type='submit' />
-          </div>
-        </form>
-
-        <h2>Todos</h2>
-        <div className='displayFlex alignStart'>
-          <label>
-            <input type='checkbox' />
-            <span>Be awesome</span>
-          </label>
-          <label>
-            <input type='checkbox' />
-            <span>Learn react</span>
-          </label>
-        </div>
       </main>
     </div>
   );
